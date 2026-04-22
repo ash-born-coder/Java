264c517f92b19705a867ac442d7a6add9a51e21d
Chanuka Ashan Gunawardena <chanukagunawardena63@gmail.com>
2026-04-23 00:06:02 +0200

    Implement overloaded constructors in Main class
    Demonstrates the use of overloaded constructors in the User class.

---
+++ practice/oop/overloadedConstructors/Main.java
@@ -0,0 +1,38 @@
+package javaCourse.overloadedConstructors;
+
+public class Main {
+    public static void main(String[] args){
+
+        // overloaded constructors = Allow a class to have multiple constructors
+        //                           with different parameter lists.
+        //                           Enable objects to be initialized in various ways
+
+        User user1 = new User("Spongebob");
+        User user2 = new User("Patrick","aol.com");
+        User user3 = new User("ASH","AWOL.com",18);
+        User user4 = new User();
+
+
+        System.out.println(user1.username);
+        System.out.println(user1.email);
+        System.out.println(user1.age);
+
+        System.out.println();
+
+        System.out.println(user2.username);
+        System.out.println(user2.email);
+        System.out.println(user2.age);
+
+        System.out.println();
+
+        System.out.println(user3.username);
+        System.out.println(user3.email);
+        System.out.println(user3.age);
+
+        System.out.println("");
+
+        System.out.println(user4.username);
+        System.out.println(user4.email);
+        System.out.println(user4.age);
+    }
+}
