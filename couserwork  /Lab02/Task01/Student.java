97704a8f4bcb2cb69260c7ad8cd3f9964cb32f44
Chanuka Ashan Gunawardena <chanukagunawardena63@gmail.com>
2026-04-22 19:12:19 +0200

    Add Student class extending Person

---
+++ couserwork  /Lab02/Task01/Student.java
@@ -0,0 +1,25 @@
+package Task1;
+
+public class Student extends Person {
+
+    private int matriculationNumber;
+    private static int nextMatriculationNumber = 1001;
+
+    public Student(String firstName, String lastName, int day, int month, int year) {
+
+        super(firstName, lastName, day, month, year);
+        this.matriculationNumber = nextMatriculationNumber++;
+
+    }
+
+    public int getMatriculationNumber() {
+        return matriculationNumber;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Matriculation number: " + matriculationNumber);
+    }
+}
+
