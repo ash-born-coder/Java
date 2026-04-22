e02af6ae9dda6318576b5145dc73e8d760d77f48
Chanuka Ashan Gunawardena <chanukagunawardena63@gmail.com>
2026-04-22 23:30:15 +0200

    Add Duck class implementing Flyable and Swimmable

---
+++ coursework/Lab03/Task03/Duck.java
@@ -0,0 +1,26 @@
+package Task03;
+
+public class Duck extends Animal implements Flyable, Swimmable{
+
+    public Duck(String name, int age){
+
+        super(name, "Duck", age);
+
+    }
+
+    @Override
+    public void makeSound(){
+        System.out.println("Quack!");
+    }
+
+    @Override
+    public void fly(){
+        System.out.println(name + " is flying over the pond!");
+    }
+
+    @Override
+    public void swim(){
+        System.out.println(name + " is swimming in the water!");
+    }
+
+}
