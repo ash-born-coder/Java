f646c89b436a245cda1e9570728b77cdb9fb1882
Chanuka Ashan Gunawardena <chanukagunawardena63@gmail.com>
2026-04-22 23:35:50 +0200

    Add AppLogger class for logging functionality

---
+++ coursework/Lab03/Task06/AppLogger.java
@@ -0,0 +1,9 @@
+package Task06;
+
+import java.util.logging.Logger;
+
+public class AppLogger {
+
+    public static final Logger LOGGER =
+            Logger.getLogger(AppLogger.class.getName());
+}
